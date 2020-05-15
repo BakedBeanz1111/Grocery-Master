609ed5e40995e0f087d283658385dd9c7c458a66
Chad <criveracrum@gmail.com>
2020-05-14 22:29:38 -0400

    unit testing examples

--- app/src/test/java/com/example/grocerymaster/MainActivityTest.java
+++ app/src/test/java/com/example/grocerymaster/MainActivityTest.java
@@ -6,10 +6,8 @@ import org.junit.Test;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 
 import static androidx.core.app.ActivityCompat.startActivityForResult;
@@ -20,34 +18,65 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
 public class MainActivityTest {
 
     @Test
-    public void testAddEmptyList() {
-    //adding list to empty mainList
-        int expected= 1;
-        int actual;
-
-
+    public void testAddList() {
+    /*This first test asserts that adding an empty list works and remains empty.
+    List is added to MainList (initially empty) and the first index of MainList is compared
+    to test List(g).
+     */
+        ArrayList<String> expected=new ArrayList<String>();
+        ArrayList<String> actual=new ArrayList<String>();//empty set of groceries
+        GroceryList firstList=new GroceryList();
+        firstList.setGroceries(actual);//adds empty set to grocery instance
+        getMainList().add(firstList);//adds grocery list to mainList
+        assertEquals(expected, firstList.getGroceries());//asserts that grocery list is empty
+        assertNotNull(getMainList());//checks that main list is not empty.
+        assertEquals(firstList, getMainList().get(0));//checks that mainList's first index is equal to test List
+    /*
+    With preconditions being set above, mainList is current not empty. Now testing adding a list with lists
+    existing.
+     */
+        GroceryList secondList=new GroceryList();//create instance of second list
+        getMainList().add(secondList);//add second list to mainList
+        assertEquals(secondList, getMainList().get(1));//check that secondList matches second index of mainList
 
     }
     @Test
-    public void testAddList(){
-        //add list to non-empty mainList
-        int expected=getMainList().size()+1;
-        int actual;
-    }
-    @Test
-    public void deleteListEmptyList(){
-        //delete list from empty mainList
-        int expected=0;
+    public void testAddToList(){
+        /*Preconditions: 1. List is initially empty and items are added.
+        2. List is non-empty and items are added.
+         */
+        ArrayList<String> expected=new ArrayList<String>(){{
+            add("banana");
+            add("orange");
+            add("muffins");
+        }};
+        ArrayList<String> actual=new ArrayList<String>(){{
+            add("banana");
+            add("orange");
+            add("muffins");
+        }};
+
+        GroceryList g=new GroceryList();
+        g.setGroceries(actual);
+        assertEquals(expected,g.getGroceries());
+
+        //Now with preconditions met for second part of test, adding more items to list
+        expected.add("pear");
+        expected.add("peach");
+        actual.add("pear");
+        actual.add("peach");
+        assertEquals(expected,g.getGroceries());
+
 
     }
 
-    @Test
-    public void deleteList(){
-        //delete list from non-empty mainList
-        int expected=getMainList().size()-1;
-    }
 
 }
