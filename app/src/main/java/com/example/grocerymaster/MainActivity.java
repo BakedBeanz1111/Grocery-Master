0511b8e3177b5835a0c971e152ecefd54d23d85a
kvnlineback <49303204+kvnlineback@users.noreply.github.com>
2020-05-19 01:02:17 -0600

    Add files via upload

--- app/src/main/java/com/example/grocerymaster/MainActivity.java
+++ app/src/main/java/com/example/grocerymaster/MainActivity.java
@@ -29,8 +29,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,6 +47,7 @@ import static android.widget.Toast.makeText;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
     private static ArrayList<GroceryList> mainList = new ArrayList<>(); //holds our list of grocery lists
+    private static ArrayList<GroceryList> deletedList = new ArrayList<>(); //holds deleted grocery lists
     private RecyclerView recyclerView; //view for showing our main list of grocery lists
     private mainListAdapter listAdapter; //adapter for updating our main list when it changes
     private int position; //keeps track of which item in the main list we clicked on
@@ -68,8 +71,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setAdapter(listAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //setDummyData(); /*populates list with arbitrary data*/
-        setTitle("Grocery Master (" + mainList.size() + ")");
+        setTitle("Groceries (" + mainList.size() + ")");
     }
+
     /*creates the options menu*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,6 +84,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static ArrayList<GroceryList> getMainList() {
         return mainList;
     }
+
     /*use to read our JSON file in our onCreate method*/
     private void readJSON() {
         try {
@@ -104,12 +109,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         g.setDate(date);
                         g.setTitle(jsonObject.getString("title"));
                         ArrayList<String> groceries = gson.fromJson(jsonObject.getString("groceries"), listType);
-                        Map<String,Boolean> groceryMap = gson.fromJson(jsonObject.getString("groceryChecks"), listType2);
-                        for(String gr: groceries) {
+                        Map<String, Boolean> groceryMap = gson.fromJson(jsonObject.getString("groceryChecks"), listType2);
+                        for (String gr : groceries) {
                             g.getGroceries().add(gr);
                         }
-                        for (Map.Entry<String,Boolean> entry : groceryMap.entrySet())   {
-                            g.getGroceryChecks().put(entry.getKey(),entry.getValue());
+                        for (Map.Entry<String, Boolean> entry : groceryMap.entrySet()) {
+                            g.getGroceryChecks().put(entry.getKey(), entry.getValue());
                         }
                         mainList.add(g);
                     }
@@ -123,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //Toast.makeText(this, "Cannot read file:" + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
     /*writes each lists' data to a json file to make persistent for later*/
     private void writeJSON() {
         JSONArray jsonArray = new JSONArray();
@@ -148,16 +154,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
+
     /*use this method to pre fill lists*/
     private void setDummyData() {
-        for(int i=1; i < 11; i++) {
+        for (int i = 1; i < 11; i++) {
             ArrayList<String> groceries = new ArrayList<>();
-            HashMap<String,Boolean> checks = new HashMap<>();
-            for(int k=1; k < 6; k++) {
+            HashMap<String, Boolean> checks = new HashMap<>();
+            for (int k = 1; k < 6; k++) {
                 String groc = "grocery item # " + k;
                 groceries.add(groc);
                 double willCheck = Math.random();
-                if(willCheck < 0.5)
+                if (willCheck < 0.5)
                     checks.put(groc, true);
                 else
                     checks.put(groc, false);
@@ -172,10 +179,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listAdapter.notifyDataSetChanged();
 
     }
+
     /*This will be called when selecting an options icon, sets up an intent based on the button clicked*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.retrieveDeleted:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose grocery list(s) to retrieve");
+                if (!deletedList.isEmpty()) {
+                    String[] lst = new String[deletedList.size()];
+                    for (int i = 0; i < lst.length; i++) {
+                        lst[i] = deletedList.get(i).getTitle();
+                    }
+                    String[] lst2 = new ArrayList<String>() {
+                        {
+                            add("add all");
+                            addAll(Arrays.asList(lst));
+                        }
+                    }.toArray(new String[0]);
+                    builder.setItems(lst2, (dialog, which) -> {
+                        if (which == 0) {
+                            mainList.addAll(deletedList);
+                            listAdapter.notifyDataSetChanged();
+                            deletedList.clear();
+                        } else {
+                            mainList.add(deletedList.get(which));
+                            deletedList.remove(which);
+                            listAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
             case R.id.about:
                 Intent intent2 = new Intent(this, AboutActivity.class);
                 startActivity(intent2);
@@ -187,29 +224,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.delete:
                 if (!mainList.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Are you sure you want to delete all Lists?");
-                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+                    builder2.setTitle("Are you sure you want to delete all Lists?");
+                    builder2.setPositiveButton("YES", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            deletedList.addAll(mainList);
                             mainList.clear();
                             listAdapter.notifyDataSetChanged();
-                            setTitle("Grocery Master (" + mainList.size() + ")");
+                            setTitle("Groceries (" + mainList.size() + ")");
                         }
                     });
-                    builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    builder2.setNegativeButton("NO", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                         }
                     });
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
+                    AlertDialog dialog2 = builder2.create();
+                    dialog2.show();
                 }
             default:
                 return super.onOptionsItemSelected(item);
         }
         return true;
     }
+
     /*Click listener for each list, will send the list as an intent to our edit activity*/
     @Override
     public void onClick(View v) {
@@ -222,8 +261,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         makeText(this, "Selected list #: " + selection.getTitle(), Toast.LENGTH_SHORT).show();
         startActivityForResult(intent, ADD_NEW_LIST);
     }
+
     /*This is our way of deleting individual grocery lists, will remove the list from our main list and
-    * tell the adapter to update the view*/
+     * tell the adapter to update the view*/
     @Override
     public boolean onLongClick(View v) {
         position = recyclerView.getChildLayoutPosition(v);
@@ -233,9 +273,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                if (!deletedList.contains(selection)) {
+                    deletedList.add(selection);
+                }
                 mainList.remove(position);
                 listAdapter.notifyDataSetChanged();
-                setTitle("Grocery Master (" + mainList.size() + ")");
+                setTitle("Groceries (" + mainList.size() + ")");
             }
         });
         builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
@@ -245,12 +288,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         AlertDialog dialog = builder.create();
         dialog.show();
-        setTitle("Grocery Master (" + mainList.size() + ")");
+        setTitle("Groceries (" + mainList.size() + ")");
         return true;
     }
+
     /*This will be called after we return from the edit activity, we will gather the new edited
-    * grocery list and put it into our main list after we remove the old one, the adapter is then called
-    * and the list view changes*/
+     * grocery list and put it into our main list after we remove the old one, the adapter is then called
+     * and the list view changes*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == ADD_NEW_LIST) {
@@ -263,23 +307,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 listAdapter.notifyDataSetChanged();
 
             }
-            setTitle("Grocery Master (" + mainList.size() + ")");
+            setTitle("Groceries (" + mainList.size() + ")");
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
     /*This method will check for duplicates before adding back into the list
-    * prevents list from keeping old list objects*/
+     * prevents list from keeping old list objects*/
     private void deleteDups(GroceryList groc) {
         ArrayList<GroceryList> temp = new ArrayList<>(mainList);
-        for(GroceryList g: temp) {
-            if(g.getId().equals(groc.getId())) {
+        for (GroceryList g : temp) {
+            if (g.getId().equals(groc.getId())) {
                 mainList.remove(g);
             }
         }
     }
 
     /*This method is called whenever the application is about to close, or before the orientation
-    * changes*/
+     * changes*/
     @Override
     protected void onPause() {
         super.onPause();
