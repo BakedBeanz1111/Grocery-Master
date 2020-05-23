0771d703660c806bf3c1be4c5f1d76e741b03be3
Amad Ali <amad.ali@sandc.com>
2020-05-22 19:08:08 -0500

    modified UI Test Cases for Pipeline

--- app/src/androidTest/java/com/example/grocerymaster/MainActivityTest.java
+++ app/src/androidTest/java/com/example/grocerymaster/MainActivityTest.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.test.espresso.DataInteraction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -17,6 +18,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -30,6 +32,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
@@ -40,16 +43,26 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest2() {
-        ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.add), withContentDescription("Add"),
+    public void mainActivityTest() {
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(R.id.action_bar),
                                         1),
-                                2),
+                                0),
                         isDisplayed()));
-        actionMenuItemView.perform(click());
+        overflowMenuButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("Add List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
 
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.lineItem1),
@@ -59,19 +72,9 @@ public class MainActivityTest {
                                         0),
                                 17),
                         isDisplayed()));
-        appCompatEditText.perform(click());
+        appCompatEditText.perform(replaceText("a"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.lineItem1),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
-                                        0),
-                                17),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("thing 1"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText3 = onView(
                 allOf(withId(R.id.lineItem2),
                         childAtPosition(
                                 childAtPosition(
@@ -79,7 +82,77 @@ public class MainActivityTest {
                                         0),
                                 8),
                         isDisplayed()));
-        appCompatEditText3.perform(replaceText("thing 2"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("b"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.lineItem3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                9),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("c"), closeSoftKeyboard());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withContentDescription("Item"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction overflowMenuButton2 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton2.perform(click());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(R.id.title), withText("Add List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.lineItem1),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                17),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("1"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.lineItem2),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                8),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("2"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.lineItem3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                9),
+                        isDisplayed()));
+        appCompatEditText6.perform(replaceText("3"), closeSoftKeyboard());
 
         ViewInteraction actionMenuItemView2 = onView(
                 allOf(withContentDescription("Item"),
@@ -91,12 +164,110 @@ public class MainActivityTest {
                         isDisplayed()));
         actionMenuItemView2.perform(click());
 
+        ViewInteraction overflowMenuButton3 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton3.perform(click());
+
+        ViewInteraction appCompatTextView3 = onView(
+                allOf(withId(R.id.title), withText("Add List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView3.perform(click());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.lineItem1),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                17),
+                        isDisplayed()));
+        appCompatEditText7.perform(replaceText("z"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.lineItem2),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                8),
+                        isDisplayed()));
+        appCompatEditText8.perform(replaceText("y"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.lineItem3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                9),
+                        isDisplayed()));
+        appCompatEditText9.perform(click());
+
+        ViewInteraction appCompatEditText10 = onView(
+                allOf(withId(R.id.lineItem3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.appcompat.widget.ContentFrameLayout")),
+                                        0),
+                                9),
+                        isDisplayed()));
+        appCompatEditText10.perform(replaceText("x"), closeSoftKeyboard());
+
+        ViewInteraction actionMenuItemView3 = onView(
+                allOf(withContentDescription("Item"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView3.perform(click());
+
+        ViewInteraction overflowMenuButton4 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton4.perform(click());
+
+        ViewInteraction appCompatTextView4 = onView(
+                allOf(withId(R.id.title), withText("Sort"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView4.perform(click());
+
+        DataInteraction appCompatTextView5 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.select_dialog_listview),
+                        childAtPosition(
+                                withId(R.id.contentPanel),
+                                0)))
+                .atPosition(0);
+        appCompatTextView5.perform(click());
+
         ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.recycle),
                         childAtPosition(
                                 withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
                                 0)));
-        recyclerView.perform(actionOnItemAtPosition(0, longClick()));
+        recyclerView.perform(actionOnItemAtPosition(2, longClick()));
 
         ViewInteraction appCompatButton = onView(
                 allOf(withClassName(is("androidx.appcompat.widget.AppCompatButton")), withText("YES"),
@@ -106,6 +277,63 @@ public class MainActivityTest {
                                         0),
                                 3)));
         appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction overflowMenuButton5 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton5.perform(click());
+
+        ViewInteraction appCompatTextView6 = onView(
+                allOf(withId(R.id.title), withText("Retrieve Deleted List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView6.perform(click());
+
+        DataInteraction appCompatTextView7 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.select_dialog_listview),
+                        childAtPosition(
+                                withId(R.id.contentPanel),
+                                0)))
+                .atPosition(1);
+        appCompatTextView7.perform(click());
+
+        ViewInteraction overflowMenuButton6 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton6.perform(click());
+
+        ViewInteraction appCompatTextView8 = onView(
+                allOf(withId(R.id.title), withText("Delete all Lists"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView8.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withClassName(is("androidx.appcompat.widget.AppCompatButton")), withText("YES"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                3)));
+        appCompatButton2.perform(scrollTo(), click());
     }
 
     private static Matcher<View> childAtPosition(
